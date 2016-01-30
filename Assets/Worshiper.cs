9a26d4c6999b5f4109cf7bac5db04e31013e64ea
hossamshuk <hossam.shuk@gmail.com>
2016-01-30 18:01:21 +0200

    added ragdoll to killed worshipers

--- Assets/Worshiper.cs
+++ Assets/Worshiper.cs
@@ -2,17 +2,27 @@
 using System.Collections;
 
 public class Worshiper : MonoBehaviour {
+    public int health;
+    public int maxHealth;
     public bool isAlive;
     public float respawnTime;
+    public Animator myAnim;
 	// Use this for initialization
 	void Start ()
     {
+        health = maxHealth;
         isAlive = true;
+        myAnim = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if(health == 0)
+        {
+            myAnim.enabled = false;
+            Invoke("Die", 4);
+        }
 	    if(!isAlive)
         {
             DisableWorshiper();
@@ -20,7 +30,11 @@ public class Worshiper : MonoBehaviour {
             
         }
 	}
-
+    public void Die()
+    {
+        isAlive = false;
+        myAnim.enabled = true;
+    }
     public IEnumerator Respawn()
     {
         yield return new WaitForSeconds(respawnTime);
@@ -42,6 +56,9 @@ public class Worshiper : MonoBehaviour {
         this.GetComponent<Collider>().enabled = true;
         this.GetComponent<MeshRenderer>().enabled = true;
         this.gameObject.transform.position = this.gameObject.GetComponentInParent<Transform>().position;
+        isAlive = true;
+        health = maxHealth;
+
     }
     
 
