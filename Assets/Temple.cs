c56533b4b44581ddd9f983e832d62f3940728c4e
hossamshuk <hossam.shuk@gmail.com>
2016-01-30 20:22:52 +0200

    set up new environment - done

--- Assets/Temple.cs
+++ Assets/Temple.cs
@@ -3,11 +3,9 @@ using System.Collections;
 
 public class Temple : MonoBehaviour {
     public float ritualMeter;
-    public int worshiperCounter;
 	// Use this for initialization
 	void Start ()
     {
-        worshiperCounter = 0;
 	}
 	
 	// Update is called once per frame
