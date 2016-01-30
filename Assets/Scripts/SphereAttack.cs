62b063f4f47f79820184e3de8552fb9b83000663
ahmadabobakr <ahmadabobakr@gmail.com>
2016-01-30 07:45:13 +0200

    fixed the ground bug + only look when charging

--- Assets/Scripts/SphereAttack.cs
+++ Assets/Scripts/SphereAttack.cs
@@ -7,6 +7,7 @@ public class SphereAttack : MonoBehaviour {
     public float maxForce;
     public bool isGrounded;
     public float jumpForce;
+    public GameObject ground;
 	// Use this for initialization
 	void Start ()
     {
@@ -19,6 +20,7 @@ public class SphereAttack : MonoBehaviour {
     {
 	    if(Input.GetMouseButtonDown(0))
         {
+
             StartCoroutine("ChargeAttack");
             myRigidbody.velocity = Vector3.zero;
             myRigidbody.useGravity = false;
@@ -47,6 +49,30 @@ public class SphereAttack : MonoBehaviour {
     {
         while(true)
         {
+            Plane playerPlane = new Plane(Vector3.up, ground.transform.position + new Vector3(0, .5f, 0));
+            // Generate a ray from the cursor position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Determine the point where the cursor ray intersects the plane.
+            // This will be the point that the object must look towards to be looking at the mouse.
+            // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+            //   then find the point along that ray that meets that distance.  This will be the point
+            //   to look at.
+            float hitdist = 0.0f;
+            // If the ray is parallel to the plane, Raycast will return false.
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                // Get the point along the ray that hits the calculated distance.
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+
+                // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                Debug.Log(targetPoint);
+
+                // Smoothly rotate towards the target point.
+                transform.rotation = targetRotation;
+            }
+
             if (attackForce < maxForce)
             {
                 attackForce += 3.5f;
