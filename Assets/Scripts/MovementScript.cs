690d4f7dab77672bbc38bb4bc44d4f0d3face166
ahmadabobakr <ahmadabobakr@gmail.com>
2016-01-30 04:40:42 +0200

    fixed movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,47 +2,63 @@
 using System.Collections;
 
 public class MovementScript : MonoBehaviour {
-    public Rigidbody myRigidbody;
+    Rigidbody myRigidbody;
     public float speed;
+    public float maxSpeed;
     public float jumpForce;
     public bool isGrounded;
+    Vector3 movementDir;
 	// Use this for initialization
 	void Start ()
     {
         isGrounded = true;
         myRigidbody = this.GetComponent<Rigidbody>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetAxis("Vertical") > 0.1)
-        {
-            this.myRigidbody.MovePosition(this.transform.position + this.transform.forward * Time.deltaTime * speed);
-        }
-        else if (Input.GetAxis("Vertical") < -0.1)
-        {
-            this.myRigidbody.MovePosition(this.transform.position + this.transform.forward * Time.deltaTime * -speed);
-        }
-
+        movementDir.x = movementDir.y = movementDir.z = 0;
+        movementDir.z = Input.GetAxis("Vertical")*speed;
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            isGrounded = false;
+            movementDir.y = jumpForce;
 
-            this.myRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+        this.myRigidbody.AddForce(movementDir, ForceMode.Impulse);
 
-
-
+        if (Mathf.Abs(this.myRigidbody.velocity.z) > maxSpeed)
+        {
+            movementDir = this.myRigidbody.velocity;
+            movementDir.z = maxSpeed*Mathf.Sign(movementDir.z);
+            this.myRigidbody.velocity= movementDir;
+        }
     }
-
-    public void OnCollisionEnter(Collision other)
+    float Damp (float speed,float maxSpeed)
+    {
+        return Mathf.Min(speed, maxSpeed);
+        
+    }
+    void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.name == "Ground")
         {
             isGrounded = true;
         }
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            myRigidbody.velocity = Vector3.zero;
+            myRigidbody.constraints = RigidbodyConstraints.None;
+            Debug.Log(other.contacts[0].point); ;
+            if (other.contacts[0].point.y < 1)
+            {
+                myRigidbody.AddForce((this.transform.position - other.transform.position) * 1000);
+            }
+        }
     }
-    public void OnCollisionExit(Collision other)
+    void OnCollisionExit(Collision other)
     {
         if (other.gameObject.name == "Ground")
         {
