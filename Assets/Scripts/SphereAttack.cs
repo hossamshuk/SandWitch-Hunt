c704494a166f615aa6292806ba035ff2d31fe9de
hossamshuk <hossam.shuk@gmail.com>
2016-01-31 07:27:53 +0200

    Team division... works at least with two players

--- Assets/Scripts/SphereAttack.cs
+++ Assets/Scripts/SphereAttack.cs
@@ -16,6 +16,13 @@ public class SphereAttack : NetworkBehaviour {
     bool mouseDown, mouseUp;
     public GameObject bloodSplatter;
     public GameObject mouseTarget;
+    TeamManager teamManager;
+    public Material blueMaterial, redMaterial;
+
+    [SyncVar]
+    public int myTeam;
+
+
 	// Use this for initialization
 	void Start ()
     {
@@ -28,12 +35,17 @@ public class SphereAttack : NetworkBehaviour {
         mouseTarget = GameObject.FindGameObjectWithTag("MouseTarget");
 		
 		transform.Translate(Vector3.up * 10);
+
+        
 			
 	}
 	
 	 void Awake()
     {
-		
+
+        teamManager = GameObject.FindGameObjectWithTag("TeamManager").GetComponent<TeamManager>();
+
+
         ground = GameObject.Find("Ground");
         chargeBar = GameObject.Find("Chargebar").GetComponent<Slider>();
         energyBar = GameObject.Find("EnergyBar").GetComponent<Slider>();
@@ -41,7 +53,38 @@ public class SphereAttack : NetworkBehaviour {
 
     public override void OnStartLocalPlayer()
     {
-        GetComponent<Renderer>().material.color = Color.green;
+         
+        if(teamManager.teamOneCounter <= teamManager.teamTwoCounter)
+        {
+            myTeam = 1;
+            teamManager.CmdRegsiterSelf(1);
+            Debug.Log("Registered self as " + myTeam);
+        }
+        else if(teamManager.teamOneCounter > teamManager.teamTwoCounter)
+        {
+            myTeam = 2;
+            teamManager.RegisterSelf(2);
+            Debug.Log("Registered self as " + myTeam);
+        }
+        else if(teamManager.teamOneCounter <= teamManager.teamTwoCounter && isClient)
+        {
+            myTeam = 1;
+            teamManager.CmdRegsiterSelf(1);
+            Debug.Log("Registered self as " + myTeam);
+        }
+
+        if(myTeam == 1)
+        {
+            this.gameObject.name = "Player 1";
+            GetComponent<Renderer>().material = blueMaterial;
+        }
+        else if(myTeam == 2)
+        {
+            this.gameObject.name = "Player 2";
+            GetComponent<Renderer>().material = redMaterial;
+        }
+
+        
     }
 	
     void Update()
@@ -137,7 +180,8 @@ public class SphereAttack : NetworkBehaviour {
 
     public void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.CompareTag("Worshiper") && myRigidbody.velocity.magnitude > 20)
+        //Divide temple prefab into two and give team numbers to differentiate
+        if(myTeam != 0 && other.gameObject.CompareTag("Worshiper"+myTeam) && myRigidbody.velocity.magnitude > 20)
         {
             other.gameObject.GetComponent<Worshiper>().health--;
             RaycastHit hit;
--- Assets/Temple.cs
+++ Assets/Temple.cs
@@ -6,6 +6,7 @@ public class Temple : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+
 	}
 	
 	// Update is called once per frame
