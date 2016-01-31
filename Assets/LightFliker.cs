dfee01d7c80675558858312247234efd8b01e9d5
hossamshuk <hossam.shuk@gmail.com>
2016-01-31 04:13:23 +0200

    Added flikering fire prefab

---
+++ Assets/LightFliker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFliker : MonoBehaviour {
+
+    Light myLight;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        myLight = this.GetComponent<Light>();
+        StartCoroutine(Fliker());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerator Fliker()
+    {
+        while(true)
+        {
+            myLight.intensity = Random.Range(3f ,5f);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
